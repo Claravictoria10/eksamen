e65c81a11574d7626aad844f607a441d71fa2266
clara <clst1000@stud.ek.dk>
2026-05-27 19:08:32 +0200

    Fået data systemet til at virke

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-//hvis denne ikke er sat til server, så kan men ikke overfører værdier url. Værdi = id
-// eks www.minside.dk/index?id=1
 export default defineConfig({
-  output: "server",
+  output: "static",
 });
